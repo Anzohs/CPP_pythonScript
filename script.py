17df81eb11737ee911c092eff782632fad9c9e21
Hugo Emanuel Ladeiro Nobre <10549215+Anzohs@users.noreply.github.com>
2025-05-26 14:17:01 +0000

    adding function to write the cpp file

--- script.py
+++ script.py
@@ -18,8 +18,28 @@ class {file}{{
 	with open(header, "w") as f:
 		f.write(content)
 
+def	create_cpp(file:str) -> None:
+	cxx = f"{file}.cpp"
+	content =f'''#include "{file}.h"
+
+{file}::{file}(void){{}}
+
+{file}::{file}(const {file}& other){{}}
+
+{file}& {file}::operator=(const {file}& other){{
+	if (this == &other)
+		return (*this);
+	return (*this);
+}}
+
+{file}::~{file}(void){{}}
+'''
+	with open(cxx, "w") as f:
+		f.write(content)
+
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		print("Wrong usage!! use the following ./script.py ClassName")
 	else:
 		create_file(sys.argv[1])
+		create_cpp(sys.argv[1])
